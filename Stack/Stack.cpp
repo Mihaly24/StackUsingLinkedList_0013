f0963ea548017b1e0cd2b34bc7a015db690fe1f2
Mihaly24 <fairuzk20@gmail.com>
2024-05-11 12:19:28 +0700

    Menambahkan pilihan case 4

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -92,6 +92,10 @@ int main()
                 cout << "Stack is empty. No top value" << endl;
             }
             break;
+
+        case 4:
+            cout << "exiting program." << endl;
+            break;
         }
     }
 }
